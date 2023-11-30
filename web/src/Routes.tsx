0851dc25a4a55f33583f23888ec64b37fa26eee8
Aditya Sridhar <aditus06@gmail.com>
2023-11-30 10:57:26 -0500

    feat: adds a login page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,6 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/login" page={LoginPage} name="login" />
       <Route path="/dashboard" page={DashboardPage} name="dashboard" />
       <Route notfound page={NotFoundPage} />
     </Router>
