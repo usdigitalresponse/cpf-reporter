e637b1b44af6b76bf2a05bf4616e15b64f8d5633
tyler <hendrickson.tsh@gmail.com>
2023-12-01 01:47:57 +0000

    Make prisma work in different hosting environments

--- api/src/lib/db.ts
+++ api/src/lib/db.ts
@@ -1,18 +1,56 @@
 // See https://www.prisma.io/docs/reference/tools-and-interfaces/prisma-client/constructor
 // for options.
 
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm'
+import { Signer } from '@aws-sdk/rds-signer'
 import { PrismaClient } from '@prisma/client'
 
 import { emitLogLevels, handlePrismaLogging } from '@redwoodjs/api/logger'
 
 import { logger } from './logger'
 
+async function rdsIAMAuthForURL(databaseURL: string): Promise<string> {
+  const url = new URL(databaseURL)
+  const signer = new Signer({
+    hostname: url.hostname,
+    port: parseInt(url.port),
+    username: url.username,
+  })
+  url.password = await signer.getAuthToken()
+  return url.toString()
+}
+
+async function ssmAuthForURL(databaseURL: string): Promise<string> {
+  const url = new URL(databaseURL)
+  const ssm = new SSMClient()
+  const response = await ssm.send(
+    new GetParameterCommand({
+      Name: process.env.DATABASE_SECRET_SSM_PARAMETER_PATH,
+      WithDecryption: true,
+    })
+  )
+  url.password = response.Parameter.Value
+  return url.toString()
+}
+
 /*
  * Instance of the Prisma Client
  */
-export const db = new PrismaClient({
-  log: emitLogLevels(['info', 'warn', 'error']),
-})
+export let db: PrismaClient
+async function createPrismaClient() {
+  let datasourceUrl: string
+  if (process.env.DATABASE_SECRET_SOURCE === 'iam') {
+    datasourceUrl = await rdsIAMAuthForURL(process.env.DATABASE_URL)
+  } else if (process.env.DATABASE_SECRET_SOURCE === 'ssm') {
+    datasourceUrl = await ssmAuthForURL(process.env.DATABASE_URL)
+  }
+
+  db = new PrismaClient({
+    log: emitLogLevels(['info', 'warn', 'error']),
+    datasourceUrl: datasourceUrl,
+  })
+}
+createPrismaClient()
 
 handlePrismaLogging({
   db,
