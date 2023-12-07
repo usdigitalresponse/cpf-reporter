d4a47c502d8db9ccce143c930d8ae4d60c14cc4f
tyler <hendrickson.tsh@gmail.com>
2023-12-07 23:57:54 +0000

    Provision an ECS cluster

---
+++ terraform/ecs_cluster.tf
@@ -0,0 +1,26 @@
+resource "aws_ecs_cluster" "default" {
+  name = var.namespace
+
+  setting {
+    name  = "containerInsights"
+    value = var.ecs_cluster_container_insights_enabled ? "enabled" : "disabled"
+  }
+
+  configuration {
+    execute_command_configuration {
+      logging = "DEFAULT"
+    }
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "default" {
+  count = length(aws_ecs_cluster.default.*)
+
+  cluster_name       = aws_ecs_cluster.default[count.index].name
+  capacity_providers = ["FARGATE"]
+}
+
+resource "aws_cloudwatch_log_group" "ecs" {
+  name_prefix       = "${var.namespace}-ecs-"
+  retention_in_days = var.log_retention_in_days
+}
