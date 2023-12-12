3eb50bd2728cbcba0e5caf82e148dc99dc349754
ifranch <inesfranch@gmail.com>
2023-12-11 20:36:24 -0800

    style: add button to users page to create new user

--- web/src/pages/User/UsersPage/UsersPage.tsx
+++ web/src/pages/User/UsersPage/UsersPage.tsx
@@ -1,7 +1,21 @@
+import Button from 'react-bootstrap/Button'
+
+import { Link, routes } from '@redwoodjs/router'
+
 import UsersCell from 'src/components/User/UsersCell'
 
 const UsersPage = () => {
-  return <UsersCell />
+  return (
+    <div>
+      <h2>Users</h2>
+      <Link to={routes['newUser']()}>
+        <Button variant="primary" className="mb-3">
+          Create New User
+        </Button>
+      </Link>
+      <UsersCell />
+    </div>
+  )
 }
 
 export default UsersPage
