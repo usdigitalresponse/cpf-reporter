d1d0a54d9bd3d4fb7f4b343d7629679e09e98df4
Aditya Sridhar <aditus06@gmail.com>
2023-12-13 15:54:56 -0500

    chore: boilerplate custom auth redwood

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -7,10 +7,12 @@ import directives from 'src/directives/**/*.{js,ts}'
 import sdls from 'src/graphql/**/*.sdl.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
 
+import { getCurrentUser } from 'src/lib/auth'
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
 
 export const handler = createGraphQLHandler({
+  getCurrentUser,
   loggerConfig: { logger, options: { requestId: true, operationName: true } },
   directives,
   sdls,
