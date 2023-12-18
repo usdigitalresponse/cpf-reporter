d26a07b536b8d712359c637f75e595db7ed9ebbc
ifranch <inesfranch@gmail.com>
2023-12-18 13:52:58 -0800

    14/View users page, add user page, edit user page (#48)
    * chore: initial commit
    
    * chore: add basic models
    
    * feat: add initial CRUD scaffolding for user
    
    * fix: add proper annotations for timestamps for user fields
    
    * style: add button to users page to create new user
    
    * feat: only show users in a specific organization
    
    * style: update style of users table and remove delete and show buttons
    
    * style: remove unnecessary columns from users table
    
    * chore: remove unused code
    
    * style: update styling of new user form
    
    * feat: make organization id optional for user and remove it from add/mutate input
    
    * chore: remove unused CSS generated by scaffolding tool
    
    * chore: fix linting issue
    
    ---------
    
    Co-authored-by: Aditya Sridhar <aditus06@gmail.com>

---
+++ web/src/components/User/UsersCell/UsersCell.tsx
@@ -0,0 +1,44 @@
+import type { FindUsersByOrganizationId } from 'types/graphql'
+
+import { Link, routes } from '@redwoodjs/router'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+import Users from 'src/components/User/Users'
+
+export const QUERY = gql`
+  query FindUsersByOrganizationId($organizationId: Int!) {
+    usersByOrganization(organizationId: $organizationId) {
+      id
+      email
+      name
+      agencyId
+      organizationId
+      roleId
+      createdAt
+      updatedAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => {
+  return (
+    <div className="rw-text-center">
+      {'No users yet. '}
+      <Link to={routes.newUser()} className="rw-link">
+        {'Create one?'}
+      </Link>
+    </div>
+  )
+}
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div className="rw-cell-error">{error?.message}</div>
+)
+
+export const Success = ({
+  usersByOrganization,
+}: CellSuccessProps<FindUsersByOrganizationId>) => {
+  return <Users usersByOrganization={usersByOrganization} />
+}
