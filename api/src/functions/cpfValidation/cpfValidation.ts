318d1bf6d14d6f184fa2915f1c1b132890413a30
Zach A. Thomas <zach@me.com>
2024-01-08 08:37:11 -0500

    create a stub implementation of the cpfValidation lambda

--- api/src/functions/cpfValidation/cpfValidation.ts
+++ api/src/functions/cpfValidation/cpfValidation.ts
@@ -1,33 +1,27 @@
-import type { APIGatewayEvent, Context } from 'aws-lambda'
+import { https } from 'https'
+
+import { S3Event, S3Handler } from 'aws-lambda'
 
 import { logger } from 'src/lib/logger'
 
-/**
- * The handler function is your code that processes http request events.
- * You can use return and throw to send a response or error, respectively.
- *
- * Important: When deployed, a custom serverless function is an open API endpoint and
- * is your responsibility to secure appropriately.
- *
- * @see {@link https://redwoodjs.com/docs/serverless-functions#security-considerations|Serverless Function Considerations}
- * in the RedwoodJS documentation for more information.
- *
- * @typedef { import('aws-lambda').APIGatewayEvent } APIGatewayEvent
- * @typedef { import('aws-lambda').Context } Context
- * @param { APIGatewayEvent } event - an object which contains information from the invoker.
- * @param { Context } context - contains information about the invocation,
- * function, and execution environment.
- */
-export const handler = async (event: APIGatewayEvent, _context: Context) => {
-  logger.info(`${event.httpMethod} ${event.path}: cpfValidation function`)
+const apiEndpoint = 'https://example.com'
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      data: 'cpfValidation function',
-    }),
+export const handler: S3Handler = async (event: S3Event): Promise<void> => {
+  try {
+    const bucket = event.Records[0].s3.bucket.name
+    const key = event.Records[0].s3.object.key
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    // call API endpoint with S3 key
+    https.request(apiEndpoint, options, (res) => {})
+  } catch (error) {
+    logger.error('Error processing S3 event:', error)
+    throw error
   }
 }
