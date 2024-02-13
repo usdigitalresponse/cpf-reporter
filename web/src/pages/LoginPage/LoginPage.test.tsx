02338f69124d7ee1c6bb520bdbaa92e125e6c24b
Aditya Sridhar <aditus06@gmail.com>
2024-02-13 10:36:48 -0500

    feat: add login page that is compatible with local auth

--- web/src/pages/LoginPage/LoginPage.test.tsx
+++ web/src/pages/LoginPage/LoginPage.test.tsx
@@ -1,3 +1,5 @@
+import { screen } from '@testing-library/react'
+
 import { render } from '@redwoodjs/testing/web'
 
 import LoginPage from './LoginPage'
@@ -6,9 +8,19 @@ import LoginPage from './LoginPage'
 //   https://redwoodjs.com/docs/testing#testing-pages-layouts
 
 describe('LoginPage', () => {
-  it('renders successfully', () => {
+  it('renders successfully for local auth', () => {
     expect(() => {
       render(<LoginPage />)
     }).not.toThrow()
+  })
+  it('renders a login button for local auth', () => {
+    render(<LoginPage />)
+    expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument()
+  })
+  it('renders an empty page when valid auth provider is missing', () => {
+    process.env.AUTH_PROVIDER = 'no-auth'
+    render(<LoginPage />)
+    const submitButton = screen.queryByText('submit')
+    expect(submitButton).not.toBeInTheDocument()
   })
 })
--- web/src/pages/LoginPage/LoginPage.tsx
+++ web/src/pages/LoginPage/LoginPage.tsx
@@ -1,19 +1,54 @@
-import { Link, routes } from '@redwoodjs/router'
+import {
+  Form,
+  EmailField,
+  Submit,
+  FieldError,
+  Label,
+  SubmitHandler,
+} from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
+import { useAuth } from 'src/auth'
+
+interface FormValues {
+  email: string
+}
+
 const LoginPage = () => {
+  const { logIn } = useAuth()
+  const onSuccess = (event: SubmitHandler<FormValues>) => {
+    logIn(event)
+  }
+  const localAuth = (
+    <>
+      <div className="rw-segment-main">
+        <div className="row mb-3">
+          <Form onSubmit={onSuccess}>
+            <Label name="email" className="form-label col-sm-2 col-form-label">
+              Email
+            </Label>
+            <div className="col-sm-6">
+              <EmailField
+                className="form-control"
+                name="email"
+                validation={{ required: true }}
+              />
+            </div>
+            <FieldError
+              name="email"
+              className="error-message offset-2 invalid-feedback"
+            />
+            <Submit className="btn btn-primary me-2">Login</Submit>
+          </Form>
+        </div>
+      </div>
+    </>
+  )
+
   return (
     <>
       <MetaTags title="Login" description="Login page" />
-
-      <h1>LoginPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/LoginPage/LoginPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>login</code>, link to me with `
-        <Link to={routes.login()}>Login</Link>`
-      </p>
+      <div>{process.env.AUTH_PROVIDER === 'local' ? localAuth : ''}</div>
     </>
   )
 }
