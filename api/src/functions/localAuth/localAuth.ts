5b5ad048726f3ef9d4258cb98d482d53bb3a50d4
Aditya Sridhar <aditus06@gmail.com>
2024-02-14 00:00:03 -0500

    feat: adds implementation of /localAuth endpoint

--- api/src/functions/localAuth/localAuth.ts
+++ api/src/functions/localAuth/localAuth.ts
@@ -1,5 +1,6 @@
 import type { APIGatewayEvent, Context } from 'aws-lambda'
 
+import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
 
 /**
@@ -18,16 +19,99 @@ import { logger } from 'src/lib/logger'
  * @param { Context } context - contains information about the invocation,
  * function, and execution environment.
  */
-export const handler = async (event: APIGatewayEvent, _context: Context) => {
-  logger.info(`${event.httpMethod} ${event.path}: localAuth function`)
 
+export interface LocalAuthResponse {
+  statusCode: number
+  headers: { [key: string]: string }
+  body?: string
+}
+
+const defaultHeaders = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': 'http://localhost:8910',
+  'Access-Control-Allow-Headers': '*',
+}
+
+const handleOptions = () => {
   return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      data: 'localAuth function',
-    }),
+    headers: defaultHeaders,
+  }
+}
+
+const handleGetUserMetadata = async (event: APIGatewayEvent) => {
+  console.log('getting user metadata')
+  let body
+  if (event.body) {
+    body = JSON.parse(event.body)
+  }
+  if (!(body && body.email)) {
+    return {
+      statusCode: 400,
+      headers: defaultHeaders,
+    }
+  }
+  const user = await db.user.findFirst({
+    where: { email: body.email },
+  })
+
+  if (user) {
+    return {
+      statusCode: 200,
+      headers: defaultHeaders,
+      body: JSON.stringify({
+        data: { user },
+      }),
+    }
+  } else {
+    return {
+      statusCode: 404,
+      headers: defaultHeaders,
+    }
+  }
+}
+
+const handlePost = (event: APIGatewayEvent) => {
+  let body
+  if (event.body) {
+    body = JSON.parse(event.body)
+  }
+
+  if (!(body && body.authMethod)) {
+    return {
+      statusCode: 400,
+      headers: defaultHeaders,
+    }
+  }
+
+  if (body.authMethod === 'getUserMetadata') {
+    return handleGetUserMetadata(event)
+  } else if (body.authMethod === 'login') {
+    // since we're not maintaining a session, we can verify the existence of the user
+    // through the use of getting the user metadata
+    return handleGetUserMetadata(event)
+  } else {
+    return {
+      statusCode: 400,
+      headers: defaultHeaders,
+    }
+  }
+}
+
+export const handler = async (
+  event: APIGatewayEvent,
+  _context: Context
+): Promise<LocalAuthResponse> => {
+  logger.info(`${event.httpMethod} ${event.path}: localAuth function`)
+
+  if (event.httpMethod === 'OPTIONS') {
+    return handleOptions()
+  } else if (event.httpMethod === 'POST') {
+    return handlePost(event)
+  } else {
+    return {
+      statusCode: 405,
+      headers: defaultHeaders,
+    }
   }
 }
