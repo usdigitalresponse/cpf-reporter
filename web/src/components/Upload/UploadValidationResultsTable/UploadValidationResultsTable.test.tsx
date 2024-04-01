897f2d88df0c8126983aa382f3e9a438690f9534
Jake Kreider <624510+jakekreider@users.noreply.github.com>
2024-04-01 13:58:22 -0500

    Upload Results Table component  (#151)
    * Basic upload results table and stories
    
    * Basic tests
    
    * Add error row test case

---
+++ web/src/components/Upload/UploadValidationResultsTable/UploadValidationResultsTable.test.tsx
@@ -0,0 +1,55 @@
+import { render } from '@redwoodjs/testing/web'
+
+import UploadValidationResultsTable from './UploadValidationResultsTable'
+import { Severity } from './UploadValidationResultsTable'
+
+describe('UploadValidationResultsTable', () => {
+  it('renders successfully when empty', () => {
+    expect(() => {
+      render(<UploadValidationResultsTable errors={[]} />)
+    }).not.toThrow()
+  })
+  it('renders successfully when given minimal results', () => {
+    expect(() => {
+      render(
+        <UploadValidationResultsTable
+          errors={[
+            {
+              message:
+                'Upload template version is older than the latest input template',
+              tab: 'Logic',
+              row: '1',
+              col: 'B',
+              severity: Severity.Warning,
+            },
+          ]}
+        />
+      )
+    }).not.toThrow()
+  })
+  it('renders successfully when given multiple results', () => {
+    expect(() => {
+      render(
+        <UploadValidationResultsTable
+          errors={[
+            {
+              message:
+                'Upload template version is older than the latest input template',
+              tab: 'Logic',
+              row: '1',
+              col: 'B',
+              severity: Severity.Warning,
+            },
+            {
+              message: 'Error: EC code must be set',
+              tab: 'Logic',
+              row: '1',
+              col: 'B',
+              severity: Severity.Error,
+            },
+          ]}
+        />
+      )
+    }).not.toThrow()
+  })
+})
