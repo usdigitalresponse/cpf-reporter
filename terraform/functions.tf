bc7474ac03f491308b52cba2542787e9f189fb48
Aditya Sridhar <aditus06@gmail.com>
2024-04-11 15:21:41 -0400

    fix: re-add mistakenly removed function trigger

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -229,6 +229,12 @@ resource "aws_s3_bucket_notification" "reporting_data" {
     filter_prefix       = "uploads/"
     filter_suffix       = ".json"
   }
+  lambda_function {
+    lambda_function_arn = module.lambda_function-cpfValidation.lambda_function_arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "uploads/"
+    filter_suffix       = ".xlsm"
+  }
 }
 
 module "lambda_function-graphql" {
