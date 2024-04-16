a4b3db1934392103f21dd228878e91e1bad63574
Aditya Sridhar <aditus06@gmail.com>
2024-04-16 13:10:26 -0400

    fix: json parsing error

--- api/src/functions/processValidationJson/processValidationJson.ts
+++ api/src/functions/processValidationJson/processValidationJson.ts
@@ -5,12 +5,15 @@ import {
 } from '@aws-sdk/client-s3'
 import { Prisma } from '@prisma/client'
 import { NodeJsClient } from '@smithy/types'
-import { S3Event, S3Handler } from 'aws-lambda'
+import {
+  S3Handler,
+  Context,
+  S3ObjectCreatedNotificationEvent,
+} from 'aws-lambda'
 
+import aws from 'src/lib/aws'
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
-
-const s3 = new S3Client({}) as NodeJsClient<S3Client>
 
 type UploadValidationRecord = {
   s3: {
@@ -22,13 +25,24 @@ type UploadValidationRecord = {
     }
   }
 }
+type Response = {
+  statusCode: number
+}
 
 type UploadValidationS3Client = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   send: (command: GetObjectCommand | DeleteObjectCommand) => Promise<any>
 }
 
-export const handler: S3Handler = async (event: S3Event): Promise<void> => {
+export const handler: S3Handler = async (
+  event: S3ObjectCreatedNotificationEvent,
+  context: Context
+): Promise<Response> => {
+  console.log(event)
+  console.log('Function name: ', context.functionName)
+  const s3 = aws.getS3Client()
+  event = event.queryStringParameters
+  console.log(event)
   await Promise.all(
     event.Records.map(async (record) => {
       try {
@@ -38,6 +52,9 @@ export const handler: S3Handler = async (event: S3Event): Promise<void> => {
       }
     })
   )
+  return {
+    statusCode: 200,
+  }
 }
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -45,23 +62,32 @@ export const processRecord = async (
   record: UploadValidationRecord,
   s3Client: UploadValidationS3Client
 ): Promise<void> => {
+  console.log(record)
   const bucket = record.s3.bucket.name
   const key = record.s3.object.key
 
   // Download the JSON errors file from S3
-  const getObjectResponse = await s3Client.send(
-    new GetObjectCommand({ Bucket: bucket, Key: key })
-  )
+  let getObjectResponse
+  try {
+    getObjectResponse = await s3Client.send(
+      new GetObjectCommand({ Bucket: bucket, Key: key })
+    )
+  } catch (err) {
+    logger.error(`Error getting object from S3: ${err} - key: ${key}`)
+    throw new Error('Error getting object from S3')
+  }
 
   // Add the contents of the JSON to the database
   if (getObjectResponse.Body) {
     /* example file path/key
          /uploads/organization_id/agency_id/reporting_period_id/upload_id/{filename}
       */
-    const result = JSON.parse(getObjectResponse.Body.toString()) || []
+    const strBody = await getObjectResponse.Body.transformToString()
+    const result = JSON.parse(strBody) || []
 
     // when the results array is empty then we know the file has passed validations
     const passed = result.length === 0
+    console.log('passed', passed)
 
     const uploadId = extractUploadIdFromKey(key)
     const input = {
@@ -69,26 +95,43 @@ export const processRecord = async (
       uploadId: uploadId,
       passed: passed,
     }
+
     // There should be an existing validation Record in the DB that will need to be updated
-    const validationRecord = await db.uploadValidation.updateMany({
-      data: input,
+    const validationRecord = await db.uploadValidation.findFirst({
       where: { uploadId, passed: false, results: { equals: Prisma.JsonNull } },
+      orderBy: { createdAt: 'desc' },
     })
-    logger.info(`Records updated: ${validationRecord.count}`)
     if (!validationRecord) {
       logger.error('Validation record not found')
       throw new Error('Validation record not found')
     }
 
-    // Delete the errors.json file from S3
-    await s3Client.send(
-      new DeleteObjectCommand({
-        Bucket: bucket,
-        Key: key,
+    try {
+      await db.uploadValidation.update({
+        data: input,
+        where: { id: validationRecord.id },
       })
-    )
+    } catch (err) {
+      logger.error(
+        `Error updating validation record: ${err} - key: ${key} - record: ${validationRecord.id}`
+      )
+      throw new Error('Error updating validation record')
+    }
+
+    // Delete the errors.json file from S3
+    try {
+      await s3Client.send(
+        new DeleteObjectCommand({
+          Bucket: bucket,
+          Key: key,
+        })
+      )
+    } catch (err) {
+      logger.error(`Error deleting object from S3: ${err} - key: ${key}`)
+      throw new Error('Error deleting object from S3')
+    }
   } else {
-    logger.warn('No body in getObjectResponse')
+    logger.error('No body in getObjectResponse')
   }
 }
 
