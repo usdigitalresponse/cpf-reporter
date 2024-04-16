0f88fbc6da46efe59bc4ee8787e9c9f7584415d8
aditya <asridhar@usdigitalresponse.org>
2024-04-16 16:20:01 -0400

    fix: file-key string from s3 (#195)
    * fix: file-key string from s3
    
    * fix: remove console log

--- api/src/functions/processValidationJson/processValidationJson.ts
+++ api/src/functions/processValidationJson/processValidationJson.ts
@@ -150,7 +150,7 @@ export const processRecord = async (
 function extractUploadIdFromKey(key: string): number {
   logger.debug(`Extracting upload_id from key: ${key}`)
   const regex =
-    /\/uploads\/(?<organization_id>\w+)\/(?<agency_id>\w+)\/(?<reporting_period_id>\w+)\/(?<upload_id>\w+)\/(?<filename>.+)/
+    /uploads\/(?<organization_id>\w+)\/(?<agency_id>\w+)\/(?<reporting_period_id>\w+)\/(?<upload_id>\w+)\/(?<filename>.+)/
   const match = key.match(regex)
   if (!match) {
     throw new Error('Invalid key format')
