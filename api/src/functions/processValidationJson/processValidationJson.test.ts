0fe9ac8a297d0c73cf787f4be21e7c89f8c79428
Laurel Korwin <lkorwin@gmail.com>
2024-04-16 21:27:43 -0700

    Set upload EC code (#199)
    * Set expenditure code on workbook validation
    
    * Python unit test

--- api/src/functions/processValidationJson/processValidationJson.test.ts
+++ api/src/functions/processValidationJson/processValidationJson.test.ts
@@ -46,7 +46,8 @@ function buildRecord(uploadValidationId: string) {
 
 describe('cpfValidation function', () => {
   scenario('no validation errors', async (scenario) => {
-    const expectedBody = JSON.stringify([])
+    const noErrorResults = { errors: [] }
+    const expectedBody = JSON.stringify(noErrorResults)
     const mocks3 = new MockS3Client(expectedBody)
     const record = buildRecord(scenario.uploadValidation.one.uploadId)
 
@@ -56,12 +57,12 @@ describe('cpfValidation function', () => {
       where: { id: scenario.uploadValidation.one.id },
     })
     expect(mocks3.commands.length).toEqual(2)
-    expect(updatedRecord.results).toEqual([])
+    expect(updatedRecord.results).toEqual(noErrorResults)
     expect(updatedRecord.passed).toEqual(true)
   })
 
   scenario('validation error', async (scenario) => {
-    const expectedBody = { error: 'error' }
+    const expectedBody = { errors: 'error' }
     const mocks3 = new MockS3Client(JSON.stringify(expectedBody))
     const record = buildRecord(scenario.uploadValidation.one.uploadId)
 
@@ -113,5 +114,15 @@ describe('cpfValidation function', () => {
     })
     expect(existingRecord.results).toEqual(null)
     expect(existingRecord.passed).toEqual(false)
+  })
+
+  scenario('no expenditure category found for result project use code', async (scenario) => {
+    const expectedResult = { errors: [], projectUseCode: '13A' }
+    const mocks3 = new MockS3Client(JSON.stringify(expectedResult))
+    const record = buildRecord(scenario.uploadValidation.one.uploadId)
+
+    expect(
+      async () => await processRecord(record, mocks3)
+    ).rejects.toThrow('Expenditure category not found')
   })
 })
