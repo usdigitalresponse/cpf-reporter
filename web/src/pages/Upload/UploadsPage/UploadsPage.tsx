9d1c92357ce08f93a7bc7b03eba59026b45cf2e3
Weronika Tomaszewska <veratomaszewska@gmail.com>
2024-04-24 14:10:18 -0400

    CPF-213 for roles other than USDR admin: hide invalidate/validate buttons, hide tabs, secure non-upload routes

--- web/src/pages/Upload/UploadsPage/UploadsPage.tsx
+++ web/src/pages/Upload/UploadsPage/UploadsPage.tsx
@@ -1,10 +1,14 @@
 import { Button, ButtonGroup } from 'react-bootstrap'
+import { useAuth } from 'web/src/auth'
+import { ROLES } from 'api/src/lib/constants'
 
 import { MetaTags } from '@redwoodjs/web'
 
 import UploadsCell from 'src/components/Upload/UploadsCell'
 
 const UploadsPage = () => {
+  const { hasRole } = useAuth()
+
   return (
     <>
       <MetaTags title="Uploads Page" description="Uploads page" />
@@ -12,9 +16,12 @@ const UploadsPage = () => {
       <h2>Uploads</h2>
 
       <ButtonGroup className="my-2 mb-4">
-        <Button size="sm" variant="primary">
-          Download Empty Template
-        </Button>
+        {/* TODO: Remove USDR_ADMIN check when ready || 2024-05-13 Milestone */}
+        {hasRole(ROLES.USDR_ADMIN) && (
+          <Button size="sm" variant="primary">
+            Download Empty Template
+          </Button>
+        )}
         <Button
           size="sm"
           variant=""
@@ -23,9 +30,12 @@ const UploadsPage = () => {
         >
           Upload Workbook
         </Button>
-        <Button size="sm" variant="" className="btn-outline-primary">
-          Send Treasury and/ Audit Reports by Email
-        </Button>
+        {/* TODO: Remove USDR_ADMIN check when ready || 2024-05-13 Milestone */}
+        {hasRole(ROLES.USDR_ADMIN) && (
+          <Button size="sm" variant="" className="btn-outline-primary">
+            Send Treasury and/ Audit Reports by Email
+          </Button>
+        )}
       </ButtonGroup>
 
       <UploadsCell />
