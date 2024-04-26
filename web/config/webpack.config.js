43de9a4992e40af9984465642398673a44038d22
Laurel Korwin <lkorwin@gmail.com>
2024-04-26 10:06:24 -0700

    Exclude deploy-config from the bundle if not in development mode

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -1,14 +1,9 @@
 /** @returns {import('webpack').Configuration} Webpack Configuration */
 
 module.exports = (config, { mode }) => {
-  if (mode === 'development') {
-    // Add dev plugin
-    if (mode === 'development') {
-      // Exclude deploy-config.js from the bundle
-      config.externals = {
-        '**/deploy-config.js': 'deployConfig',
-      }
-    }
+  if (mode != 'development') {
+    // Exclude deploy-config.js from the bundle if we are not developing locally
+    config.externals = /^(.*\/public\/deploy-config\.js|\$)$/i
   }
   return config
 }
