3b817d0584073d0881e346a78389d2cdef74e327
Laurel Korwin <lkorwin@gmail.com>
2024-04-26 10:35:24 -0700

    Add script tag for deploy-config and make things work locally (#230)

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,10 @@ import dns from 'dns'
 import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
 
+// NOTE TO DEVELOPERS: This config is not actually being applied at the moment, as webpack is still specified
+// as the bundler in redwood.toml. When we update the project to use vite as the bundler, please remove this comment
+// and also delete the webpack.config.js file :)
+
 // See: https://vitejs.dev/config/server-options.html#server-host
 // So that Vite will load on local instead of 127.0.0.1
 dns.setDefaultResultOrder('verbatim')
