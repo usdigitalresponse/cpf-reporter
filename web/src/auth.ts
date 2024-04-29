c6f7297b91e07d20ba052fe4fdbd8082347c784e
Weronika Tomaszewska <veratomaszewska@gmail.com>
2024-04-28 20:25:50 -0400

    CPF-122 check window.APP_CONFIG when determining if we should use Passage authentication

--- web/src/auth.ts
+++ web/src/auth.ts
@@ -10,13 +10,13 @@ import {
 } from 'src/auth/passageAuth'
 
 function createAuth() {
-  if (process.env.AUTH_PROVIDER == 'local') {
+  if (
+    window.APP_CONFIG?.webConfigParams?.AUTH_PROVIDER == 'local' ||
+    process.env.AUTH_PROVIDER == 'local'
+  ) {
     const authImplementation = createLocalAuthImplementation(localAuthClient)
     return createAuthentication(authImplementation)
-  } else if (
-    process.env.AUTH_PROVIDER == 'passage' ||
-    window.APP_CONFIG?.webConfigParams?.AUTH_PROVIDER == 'passage'
-  ) {
+  } else if (window.APP_CONFIG?.webConfigParams?.AUTH_PROVIDER == 'passage') {
     const authImplementation =
       createPassageAuthImplementation(passageAuthClient)
     return createAuthentication(authImplementation)
