bf908a073695871d50e3b752238b90952b7fda5a
Victor Shia <401047+vshia@users.noreply.github.com>
2024-05-28 10:43:04 -0700

    chore: make example script more readable

--- scripts/tests3.ts
+++ scripts/tests3.ts
@@ -1,48 +1,83 @@
 import {
-  GetObjectCommand,
   CreateBucketCommand,
+  GetObjectCommand,
+  GetObjectCommandInput,
   HeadObjectCommand,
   HeadObjectCommandInput,
   PutObjectCommand,
   PutObjectCommandInput,
-  S3Client,
+  DeleteObjectCommand,
+  DeleteObjectCommandInput,
+  DeleteBucketCommand,
+  DeleteBucketCommandInput,
 } from '@aws-sdk/client-s3'
 import { getSignedUrl as awsGetSignedUrl } from '@aws-sdk/s3-request-presigner'
-import type { Prisma } from '@prisma/client'
-import { db, getPrismaClient } from 'api/src/lib/db'
+import { getPrismaClient } from 'api/src/lib/db'
 import AWS from 'api/src/lib/aws'
 
 const REPORTING_DATA_BUCKET_NAME = `${process.env.REPORTING_DATA_BUCKET_NAME}`
 
 export default async () => {
+  const s3 = AWS.getS3Client()
+  await getPrismaClient()
+  const key = "12345"
+
+  console.log("Create bucket")
   try {
-    await getPrismaClient()
-
-    const s3 = AWS.getS3Client()
-    const key = "12345"
-    const baseParams: PutObjectCommandInput = {
-      Bucket: REPORTING_DATA_BUCKET_NAME,
-      Key: key,
-      ContentType: 'application/vnd.ms-excel.sheet.macroenabled.12',
-      ServerSideEncryption: 'AES256',
-    }
-    const url = await awsGetSignedUrl(s3, new PutObjectCommand(baseParams), {
-      expiresIn: 60,
-    })
-    console.log("url", url)
-
-    const uploadParams: HeadObjectCommandInput = {
-      Bucket: REPORTING_DATA_BUCKET_NAME,
-      Key: key,
-    }
     await s3.send(new CreateBucketCommand({
       Bucket: REPORTING_DATA_BUCKET_NAME,
     }))
-    await s3.send(new HeadObjectCommand(uploadParams))
-  
+    console.log("-- Bucket created")
+  } catch (error) {
+    if (error.Code == "BucketAlreadyOwnedByYou") {
+      console.log("-- Bucket already exists")
+    } else {
+      console.log(error)
+    }
+  }
 
+  const baseParams: GetObjectCommandInput = {
+    Bucket: REPORTING_DATA_BUCKET_NAME,
+    Key: key,
+  }
+  try {
+    console.log("Create signed URL")
+    const url = await awsGetSignedUrl(s3, new GetObjectCommand(baseParams), {
+      expiresIn: 60,
+    })
+    console.log("-- signed url", url)
+
+    console.log("Upload file")
+    const uploadParams: PutObjectCommandInput = {
+      Body: "tests3.ts",  // Upload this file as a test
+      Bucket: REPORTING_DATA_BUCKET_NAME,
+      Key: key,
+    }
+    await s3.send(new PutObjectCommand(uploadParams))
+
+    console.log("-- Look at uploaded file")
+    const headParams: HeadObjectCommandInput = {
+      Bucket: REPORTING_DATA_BUCKET_NAME,
+      Key: key,
+    }
+    const response = await s3.send(new HeadObjectCommand(headParams))
+    console.log("--", response)
+
+    console.log("Delete file")
+    const deleteParams: DeleteObjectCommandInput = {
+      Bucket: REPORTING_DATA_BUCKET_NAME,
+      Key: key,
+    }
+    await s3.send(new DeleteObjectCommand(headParams))
+    console.log("-- file deleted")
   } catch (error) {
     console.warn('Failed to test s3.')
     console.error(error)
+  } finally {
+    console.log("Remove bucket")
+    await s3.send(new DeleteBucketCommand({
+      Bucket: REPORTING_DATA_BUCKET_NAME,
+    } as DeleteBucketCommandInput))
+    console.log("-- Bucket removed")
   }
 }
