c0178b36b2970a12d31aaffc569d76fcbba8262d
Laurel Korwin <lkorwin@gmail.com>
2024-06-03 12:19:17 -0700

    Migration script to add current reporting period id

---
+++ api/db/migrations/20240603182838_populate_organization_preferences/migration.sql
@@ -0,0 +1,31 @@
+-- This is an empty migration.
+BEGIN;
+
+CREATE OR REPLACE FUNCTION populate_organization_preferences()
+RETURNS void AS $$
+DECLARE 
+    current_reporting_period_id INT;
+BEGIN 
+    -- Get one valid reporting period id --
+    SELECT id INTO current_reporting_period_id 
+    FROM "ReportingPeriod" 
+    WHERE "outputTemplateId" IS NOT NULL AND "inputTemplateId" IS NOT NULL
+    LIMIT 1;
+
+    -- Update preferences to use new reporting period id --
+    IF current_reporting_period_id IS NOT NULL THEN
+        UPDATE "Organization"
+        SET preferences = COALESCE(
+            jsonb_set(preferences, '{current_reporting_period_id}', to_jsonb(current_reporting_period_id)),
+            jsonb_build_object('current_reporting_period_id', current_reporting_period_id)
+        )
+        WHERE preferences IS NULL OR (preferences->'current_reporting_period_id')::text = 'null';
+    ELSE
+        RAISE NOTICE 'No valid current reporting period ID found';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT populate_organization_preferences();
+
+COMMIT;
