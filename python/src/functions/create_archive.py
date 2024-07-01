800b073ed0da00f9a8d7a0f432e2b1af921615b2
Jake Kreider <jake@jakekreider.com>
2024-06-30 19:18:07 -0500

    First draft at function and script for testing with localstack

---
+++ python/src/functions/create_archive.py
@@ -0,0 +1,75 @@
+import json
+import os
+import tempfile
+import zipfile
+
+import boto3
+from aws_lambda_typing.context import Context
+from aws_lambda_typing.events import SQSEvent
+from mypy_boto3_s3.client import S3Client
+from src.lib.logging import get_logger, reset_contextvars
+
+S3_BUCKET = os.environ["S3_BUCKET"]
+
+
+@reset_contextvars
+def handle(event: SQSEvent, _context: Context):
+    """Lambda handler for creating an archive of CSV files in S3
+
+    Args:
+        event: SQS event containing the org_id and reporting_period_id
+        context: Lambda context
+    """
+    logger = get_logger()
+    logger.info("Received new invocation event from SQS")
+    logger.info(event)
+    logger.info("Extracting payload")
+    payload = json.loads(event["Records"][0]["body"])
+    logger.info(payload)
+    reporting_period_id = payload["reporting_period_id"]
+    org_id = payload["org_id"]
+    logger.info(
+        f"Creating archive for org_id: {org_id}, reporting_period_id: {reporting_period_id}"
+    )
+    create_archive(org_id, reporting_period_id, boto3.client("s3"), logger)
+
+
+def create_archive(
+    org_id: str, reportiong_period_id: str, s3_client: S3Client, logger=None
+):
+    """Create a zip archive of CSV files in S3"""
+
+    if logger is None:
+        logger = get_logger()
+
+    target_key = f"treasuryreports/{org_id}/{reportiong_period_id}/"
+    logger.info(f"Creating archive for {target_key}")
+    # find all file names in the target key with the CSV suffix
+    paginator = s3_client.get_paginator("list_objects_v2")
+    response_iterator = paginator.paginate(Bucket=S3_BUCKET, Prefix=target_key)
+    target_files = []
+    for response in response_iterator:
+        for obj in response.get("Contents", []):
+            if obj["Key"].endswith(".csv"):
+                file_name = obj["Key"]
+                logger.info(f"Found CSV file: {file_name}")
+                target_files.append(file_name)
+    if not target_files:
+        logger.info("No CSV files found in the target key")
+        return
+
+    # create a zip file with all the CSV files
+    with tempfile.NamedTemporaryFile() as file:
+        with zipfile.ZipFile(file, "w") as zipf:
+            for target_file in target_files:
+                obj = s3_client.get_object(Bucket=S3_BUCKET, Key=target_file)
+                zipf.writestr(target_file, obj["Body"].read())
+
+            zipf.close()
+            file.flush()
+            logger.info(f"Created archive file: {file.name}")
+            upload_key = f"{target_key}report.zip"
+            # upload the zip file to the target key
+            s3_client.upload_file(file.name, S3_BUCKET, upload_key)
+            logger.info(f"Uploaded archive file to {upload_key}")
+            logger.info(f"Uploaded archive file to {upload_key}")
---
+++ python/test-create-archive.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+AWS_PAGER=""
+
+# generate a random prefix for resources
+RS_PREFIX=$(date +%s)
+QUEUE_NAME=${RS_PREFIX}-my-queue
+S3_BUCKET=${RS_PREFIX}-cpf-reporter
+LAMBDA_HANDLER=src.functions.create_archive.handle
+LAMBDA_ZIP=/Users/jakekreider/projects/cpf-reporter/python/dist/lambda.zip
+LAMBDA_NAME=${RS_PREFIX}-create-archive
+TEST_ORG_ID=123
+TEST_REPORTING_PERIOD_ID=456
+TEST_KEY_PATH=treasuryreports/${TEST_ORG_ID}/${TEST_REPORTING_PERIOD_ID}/
+
+# Create an SQS queue
+awslocal sqs create-queue --queue-name ${QUEUE_NAME}
+
+# create the bucket
+awslocal s3 mb s3://${S3_BUCKET}
+
+# create the lambda
+ awslocal lambda create-function --function-name ${LAMBDA_NAME} --runtime python3.12 --handler ${LAMBDA_HANDLER} --role arn:aws:iam::000000000000:role/irrelevant --zip-file fileb://${LAMBDA_ZIP} --environment Variables="{S3_BUCKET=${S3_BUCKET}}"
+
+awslocal lambda create-event-source-mapping --function-name ${LAMBDA_NAME} --batch-size 1 --event-source-arn arn:aws:sqs:us-west-2:000000000000:${QUEUE_NAME} --starting-position LATEST
+
+# Add fake files to the bucket
+touch ${RS_PREFIX}file1.csv
+awslocal s3 cp ${RS_PREFIX}file1.csv s3://${S3_BUCKET}/${TEST_KEY_PATH}
+rm ${RS_PREFIX}file1.csv
+# Send a message to the queue with fields org_id, reporting_period_id
+awslocal sqs send-message --queue-url http://sqs.us-west-2.localhost.localstack.cloud:4566/000000000000/${QUEUE_NAME} --message-body "{\"org_id\": ${TEST_ORG_ID}, \"reporting_period_id\": ${TEST_REPORTING_PERIOD_ID}}"
+
+# invoke the function manually
+awslocal lambda invoke --cli-binary-format raw-in-base64-out --function-name ${LAMBDA_NAME} --payload "{\"org_id\": ${TEST_ORG_ID}, \"reporting_period_id\": ${TEST_REPORTING_PERIOD_ID}}" /dev/stdout
+
+# Get the function logs
+awslocal logs tail /aws/lambda/${LAMBDA_NAME}
+
+# check for the output in the bucket
+awslocal s3 ls s3://${S3_BUCKET}/${TEST_KEY_PATH}
+
+# clean-up 
+awslocal lambda delete-function --function-name ${LAMBDA_NAME}
+awslocal s3 rb s3://${S3_BUCKET} --force
+awslocal sqs delete-queue --queue-url http://sqs.us-west-2.localhost.localstack.cloud:4566/000000000000/${QUEUE_NAME}
