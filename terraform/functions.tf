75cee1b141984017da282ca7a39d08be0f5e05e8
Laurel Korwin <lkorwin@gmail.com>
2024-07-17 13:31:37 -0700

    Adding skeleton lambda for subrecipient file gen (#366)
    * Adding skeleton lambda for subrecipient file gen
    
    * Update python/src/functions/subrecipient_treasury_report_gen.py
    
    Co-authored-by: Tyler Hendrickson <hendrickson.tsh@gmail.com>
    
    * Update terraform/functions.tf
    
    Co-authored-by: Tyler Hendrickson <hendrickson.tsh@gmail.com>
    
    * update vscode settings
    
    ---------
    
    Co-authored-by: Tyler Hendrickson <hendrickson.tsh@gmail.com>

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -515,3 +515,77 @@ module "lambda_function-cpfValidation" {
     }
   }
 }
+
+module "lambda_function-subrecipientTreasuryReportGen" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "6.5.0"
+
+  // Metadata
+  function_name = "${var.namespace}-subrecipientTreasuryReportGen"
+  description   = "Generates subrecipients file for treasury report when called by step function."
+
+  // Networking
+  vpc_subnet_ids         = null
+  vpc_security_group_ids = null
+  attach_network_policy  = false
+
+  // Permissions
+  role_permissions_boundary         = local.permissions_boundary_arn
+  attach_cloudwatch_logs_policy     = true
+  cloudwatch_logs_retention_in_days = var.log_retention_in_days
+  attach_policy_jsons               = length(local.lambda_default_execution_policies) > 0
+  number_of_policy_jsons            = length(local.lambda_default_execution_policies)
+  policy_jsons                      = local.lambda_default_execution_policies
+  attach_policy_statements          = true
+  policy_statements = {
+    AllowDownloadSubrecipientsFile = {
+      effect = "Allow"
+      actions = [
+        "s3:GetObject",
+        "s3:HeadObject",
+      ]
+      resources = [
+        # Path: /{organization_id}/{reporting_period_id}/subrecipients
+        "${module.reporting_data_bucket.bucket_arn}/*/*/subrecipients",
+      ]
+    }
+    AllowUploadCSVReport = {
+      effect = "Allow"
+      actions = [
+        "s3:PutObject"
+      ]
+      resources = [
+        # Path: /treasuryreports/{organization_id}/{reporting_period_id}/{userId}/CPFSubrecipientTemplate.csv
+        "${module.reporting_data_bucket.bucket_arn}/treasuryReports/*/*/*/CPFSubrecipientTemplate.csv",
+      ]
+    }
+  }
+
+  // Artifacts
+  publish        = true
+  create_package = false
+  s3_existing_package = {
+    bucket = aws_s3_object.lambda_artifact-python.bucket
+    key    = aws_s3_object.lambda_artifact-python.key
+  }
+
+  // Runtime
+  handler       = var.datadog_enabled ? local.datadog_lambda_py_handler : "src.functions.subrecipient_treasury_report_gen.handle"
+  runtime       = var.lambda_py_runtime
+  architectures = [var.lambda_arch]
+  layers        = local.lambda_py_layer_arns
+  timeout       = 60 # 1 minute, in seconds
+  memory_size   = 512
+  environment_variables = merge(local.lambda_default_environment_variables, {
+    DD_LAMBDA_HANDLER = "src.functions.subrecipient_treasury_report_gen.handle"
+    DD_LOGS_INJECTION = "true"
+  })
+
+  // Triggers -- TODO uncomment the below when step function code is added
+  # allowed_triggers = {
+  #   StepFunctionTrigger = {
+  #     principal  = "states.amazonaws.com"
+  #     source_arn = put_correct_trigger_here.arn
+  #   }
+  # }
+}
