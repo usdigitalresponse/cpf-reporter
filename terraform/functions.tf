48f69f58451eff2fe3a13e100b58766897c19834
Victor Shia <401047+vshia@users.noreply.github.com>
2024-07-19 09:01:38 -0700

    chore: run terraform fmt

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -518,8 +518,8 @@ module "lambda_function-cpfValidation" {
 
 module "lambda_function-treasuryReportGeneration" {
   for_each = toset(["1A", "1B", "1C"])
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "6.5.0"
+  source   = "terraform-aws-modules/lambda/aws"
+  version  = "6.5.0"
 
   // Metadata
   function_name = "${var.namespace}-treasuryReportGeneration${each.key}"
@@ -600,7 +600,7 @@ module "lambda_function-treasuryReportGeneration" {
   environment_variables = merge(local.lambda_default_environment_variables, {
     DD_LAMBDA_HANDLER = "src.functions.generate_treasury_report.handle"
     DD_LOGS_INJECTION = "true"
-    PROJECT_USE_CODE = "${each.key}"
+    PROJECT_USE_CODE  = "${each.key}"
   })
 
   // Triggers
