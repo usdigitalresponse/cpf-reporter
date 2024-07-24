0fb3d762ae26da86a028308a386cda10532ca4a0
Victor Shia <victor@humandx.org>
2024-07-24 10:55:52 -0700

    Generate treasury report for Projects 1A, 1B, 1C (#338)
    * schema: Add output column for 1C
    
    * chore: return projects and subrecipients on parameter
    
    * chore: add str functions to enums
    
    * chore: add input test files and output treasury templates
    
    * chore: add tests
    
    * chore: generate treasury report for 1C
    
    * chore: update csv file generation
    
    * chore: Update validate
    
    * chore: Update lint
    
    * chore: Add output rows for Project 1A
    
    * chore: Add output rows for Project 1B
    
    * chore: line schema
    
    * chore: generalize treasure report code
    
    * chore: generalize treasury report code
    
    * chore: fix tests
    
    * chore: get upload to work
    
    * chore: add tests for 1a and 1b
    
    * chore: add output templates for 1a and 1b
    
    * chore: Update test for output template
    
    * chore: Update version
    
    * chore: rename output column to treasury_report
    
    * chore: change latest to active version
    
    * chore: update function with new logic
    
    * fix: lint issues
    
    * fix: minor fix to variable name
    
    * Lint commits
    
    * seed: update seed file with reporting period
    
    * chore: add lambda function for project 1A
    
    * chore: make lambda function for each project code
    
    * chore: fix function
    
    * chore: run terraform fmt
    
    * chore: remove user.id in container name
    
    * chore: remove user.id from path
    
    ---------
    
    Co-authored-by: Victor Shia <401047+vshia@users.noreply.github.com>

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -589,3 +589,99 @@ module "lambda_function-subrecipientTreasuryReportGen" {
   #   }
   # }
 }
+
+module "lambda_function-treasuryReportGeneration" {
+  for_each = toset(["1A", "1B", "1C"])
+  source   = "terraform-aws-modules/lambda/aws"
+  version  = "6.5.0"
+
+  // Metadata
+  function_name = "${var.namespace}-treasuryReportGeneration${each.key}"
+  description   = "Creates the Treasury Report for Projects ${each.key}."
+
+  // Networking
+  vpc_subnet_ids         = null
+  vpc_security_group_ids = null
+  attach_network_policy  = false
+
+  // Permissions
+  role_permissions_boundary         = local.permissions_boundary_arn
+  attach_cloudwatch_logs_policy     = true
+  cloudwatch_logs_retention_in_days = var.log_retention_in_days
+  attach_policy_jsons               = length(local.lambda_default_execution_policies) > 0
+  number_of_policy_jsons            = length(local.lambda_default_execution_policies)
+  policy_jsons                      = local.lambda_default_execution_policies
+  attach_policy_statements          = true
+  policy_statements = {
+    AllowDownloadExcelObjects = {
+      effect = "Allow"
+      actions = [
+        "s3:GetObject",
+        "s3:HeadObject",
+      ]
+      resources = [
+        # Path: treasuryreports/{organization_id}/{reporting_period_id}/{filename}.xlsm
+        "${module.reporting_data_bucket.bucket_arn}/treasuryreports/*/*/*.xlsm",
+      ]
+    }
+    AllowUploadXlsxObjects = {
+      effect = "Allow"
+      actions = [
+        "s3:PutObject"
+      ]
+      resources = [
+        # Path: uploads/{organization_id}/{reporting_period_id}/{filename}.xlsx
+        "${module.reporting_data_bucket.bucket_arn}/uploads/*/*/*.xlsx",
+      ]
+    }
+    AllowUploadCsvObjects = {
+      effect = "Allow"
+      actions = [
+        "s3:PutObject"
+      ]
+      resources = [
+        # Path: uploads/{organization_id}/{reporting_period_id}/{filename}.csv
+        "${module.reporting_data_bucket.bucket_arn}/uploads/*/*/*.csv",
+      ]
+    }
+    AllowUploadJsonObjects = {
+      effect = "Allow"
+      actions = [
+        "s3:PutObject"
+      ]
+      resources = [
+        # Path: uploads/{organization_id}/{reporting_period_id}/{filename}.json
+        "${module.reporting_data_bucket.bucket_arn}/uploads/*/*/*.json",
+      ]
+    }
+  }
+
+  // Artifacts
+  publish        = true
+  create_package = false
+  s3_existing_package = {
+    bucket = aws_s3_object.lambda_artifact-python.bucket
+    key    = aws_s3_object.lambda_artifact-python.key
+  }
+
+  // Runtime
+  handler       = var.datadog_enabled ? local.datadog_lambda_py_handler : "src.functions.generate_treasury_report.handle"
+  runtime       = var.lambda_py_runtime
+  architectures = [var.lambda_arch]
+  layers        = local.lambda_py_layer_arns
+  timeout       = 60 # 1 minute, in seconds
+  memory_size   = 512
+  environment_variables = merge(local.lambda_default_environment_variables, {
+    DD_LAMBDA_HANDLER = "src.functions.generate_treasury_report.handle"
+    DD_LOGS_INJECTION = "true"
+    PROJECT_USE_CODE  = "${each.key}"
+  })
+
+  // Triggers
+  allowed_triggers = {
+    S3BucketNotification = {
+      principal  = "s3.amazonaws.com"
+      source_arn = module.reporting_data_bucket.bucket_arn
+    }
+  }
+}
