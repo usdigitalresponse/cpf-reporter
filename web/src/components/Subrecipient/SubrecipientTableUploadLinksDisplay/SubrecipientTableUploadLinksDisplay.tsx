82dcda3a9d255d124f36c1eeccbb4e78d6c54fea
Weronika Tomaszewska <veratomaszewska@gmail.com>
2024-09-27 10:42:23 -0400

    add renderUploadSection func to clean up styling, add types, linting

--- web/src/components/Subrecipient/SubrecipientTableUploadLinksDisplay/SubrecipientTableUploadLinksDisplay.tsx
+++ web/src/components/Subrecipient/SubrecipientTableUploadLinksDisplay/SubrecipientTableUploadLinksDisplay.tsx
@@ -2,6 +2,10 @@ import { Subrecipient } from 'types/graphql'
 
 import { Link, routes } from '@redwoodjs/router'
 
+type SubrecipientUpload =
+  | Subrecipient['validSubrecipientUploads'][number]
+  | Subrecipient['invalidSubrecipientUploads'][number]
+
 interface SubrecipientTableUploadLinksDisplayProps {
   validSubrecipientUploads: Subrecipient['validSubrecipientUploads']
   invalidSubrecipientUploads: Subrecipient['invalidSubrecipientUploads']
@@ -11,50 +15,43 @@ const SubrecipientTableUploadLinksDisplay = ({
   validSubrecipientUploads,
   invalidSubrecipientUploads,
 }: SubrecipientTableUploadLinksDisplayProps) => {
-  const renderUploadLink = (upload) => (
-    <Link
-      key={upload.id}
-      to={routes.upload({ id: upload.upload.id })}
-      title={`Show upload ${upload.upload.id} detail`}
-      className="link-underline link-underline-opacity-0"
-    >
-      Upload {upload.id}
-    </Link>
+  const renderUploadLink = (subrecipientUpload: SubrecipientUpload) => {
+    const { id, upload } = subrecipientUpload
+    return (
+      <Link
+        key={id}
+        to={routes.upload({ id: upload.id })}
+        title={`Show upload ${upload.id} detail`}
+        className="d-block link-underline link-underline-opacity-0"
+      >
+        Upload {upload.id}
+      </Link>
+    )
+  }
+
+  const renderUploadSection = (
+    title: string,
+    uploads: SubrecipientUpload[]
+  ) => (
+    <>
+      <div className="fw-bold">{title}</div>
+      <div className="mb-3">{uploads.map(renderUploadLink)}</div>
+    </>
   )
 
   return (
     <>
       {validSubrecipientUploads.length > 0 &&
-        validSubrecipientUploads[0].upload && (
-          <>
-            <div className="fw-bold">Latest Valid Upload:</div>
-            <div className="mb-3">
-              {renderUploadLink(validSubrecipientUploads[0]).upload}
-            </div>
-          </>
+        renderUploadSection('Latest Valid Upload:', [
+          validSubrecipientUploads[0],
+        ])}
+      {validSubrecipientUploads.length > 1 &&
+        renderUploadSection(
+          'Other Valid Uploads:',
+          validSubrecipientUploads.slice(1)
         )}
-
-      {validSubrecipientUploads.length > 1 && (
-        <>
-          <div className="fw-bold">Other Valid Uploads:</div>
-          <div className="mb-3">
-            {validSubrecipientUploads
-              .slice(1)
-              .map((upload) => renderUploadLink(upload))}
-          </div>
-        </>
-      )}
-
-      {invalidSubrecipientUploads.length > 0 && (
-        <>
-          <div className="fw-bold">Invalid Uploads:</div>
-          <div>
-            {invalidSubrecipientUploads.map((upload) =>
-              renderUploadLink(upload)
-            )}
-          </div>
-        </>
-      )}
+      {invalidSubrecipientUploads.length > 0 &&
+        renderUploadSection('Invalid Uploads:', invalidSubrecipientUploads)}
     </>
   )
 }
