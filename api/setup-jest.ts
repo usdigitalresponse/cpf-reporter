8a097359683bc6409c4d499b4e7f61627f4f91d0
Greg Adams <41639787+greg-adams@users.noreply.github.com>
2024-10-03 12:13:59 -0700

    Feat: Reporting Period ID column (#473)
    * linting
    
    * add id column

--- api/setup-jest.ts
+++ api/setup-jest.ts
@@ -2,6 +2,6 @@ import initializeDb from './src/lib/db'
 
 beforeAll(async () => {
   // explicit set to avoid conflict in docker container
-  process.env.NODE_ENV = 'test';
+  process.env.NODE_ENV = 'test'
   await initializeDb()
 })
