dccfad0b6c4d09c6d945ffc4265bde885f4bd723
Greg Adams <41639787+greg-adams@users.noreply.github.com>
2024-11-15 12:07:12 -0800

    add new btn (#499)

--- web/src/pages/OutputTemplate/OutputTemplatesPage/OutputTemplatesPage.tsx
+++ web/src/pages/OutputTemplate/OutputTemplatesPage/OutputTemplatesPage.tsx
@@ -1,7 +1,17 @@
+import { Link, routes } from '@redwoodjs/router'
+
 import OutputTemplatesCell from 'src/components/OutputTemplate/OutputTemplatesCell'
 
 const OutputTemplatesPage = () => {
-  return <OutputTemplatesCell />
+  return (
+    <div>
+      <h2>Output Templates</h2>
+      <Link to={routes.newOutputTemplate()} className="btn btn-primary mb-4">
+        Create New Output Template
+      </Link>
+      <OutputTemplatesCell />
+    </div>
+  )
 }
 
 export default OutputTemplatesPage
